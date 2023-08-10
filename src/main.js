039936377c47c7b1eb5fbda9f15bc55de2a8bf38
SeonhyeY <syoon330@gmail.com>
2023-08-10 17:27:10 -0600

    filtering items by clicking icons

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ function displayItems(items) {
   container.innerHTML = items.map((item) => createHTMLString(item)).join('');
 }
 
+// Create HTML list item from the given data item
 function createHTMLString(item) {
   return `
     <li class="item">
@@ -20,9 +21,30 @@ function createHTMLString(item) {
   `;
 }
 
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  const filtered = items.filter((item) => item[key] === value);
+  displayItems(filtered);
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const btns = document.querySelector('.btns');
+
+  logo.addEventListener('click', () => displayItems(items));
+  btns.addEventListener('click', (event) => onButtonClick(event, items));
+}
+
 loadItems()
   .then((items) => {
     displayItems(items);
-    // setEventListeners(items);
+    setEventListeners(items);
   })
   .catch(console.log);
