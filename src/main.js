72f87e5a9943ec3b666e3fdbfe8b6bbb92381181
SeonhyeY <syoon330@gmail.com>
2023-08-10 16:49:24 -0600

    display fetched data on page

--- src/main.js
+++ src/main.js
@@ -5,9 +5,24 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+// Update the list with the given items
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map((item) => createHTMLString(item)).join('');
+}
+
+function createHTMLString(item) {
+  return `
+    <li class="item">
+      <img class="item__thumbnail" src="${item.image}" alt="${item.type}" />
+      <span class="item__desciprtion">${item.gender}, ${item.size}</span>
+    </li>
+  `;
+}
+
 loadItems()
   .then((items) => {
-    // displayItems(items);
+    displayItems(items);
     // setEventListeners(items);
   })
   .catch(console.log);
